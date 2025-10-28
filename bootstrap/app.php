0638ad58488512cffb22e15d66dbac3bda63168f
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-10-28 16:19:20 +0100

    Paso 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+                  ->withRouting(
+                      web: __DIR__.'/../routes/web.php',
+                      api: __DIR__.'/../routes/api.php',
+                      commands: __DIR__.'/../routes/console.php',
+                      health: '/up',
+                  )
+                  ->withMiddleware(function (Middleware $middleware): void {
+                      $middleware->prependToGroup('api', \App\Http\Middleware\AlwaysAcceptJsonMiddleware::class);
+                  })
+                  ->withExceptions(function (Exceptions $exceptions): void {
+                      $exceptions->renderable(function (NotFoundHttpException $e, Request $request) {
+                          if ($request->wantsJson()) {
+                              return response()->json(['message' => 'Objetct Not Found'], 404);
+                          }
+                      });
+                  })->create();
